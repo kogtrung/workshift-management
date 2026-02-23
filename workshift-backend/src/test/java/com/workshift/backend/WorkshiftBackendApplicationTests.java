b65e184e91be7e22af82100509b477cf6f6ae11d
kogtrung <ct34bgithub@gmail.com>
2026-02-24 03:28:15 +0700

    initialize project structure

---
+++ workshift-backend/src/test/java/com/workshift/backend/WorkshiftBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshift.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshiftBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
